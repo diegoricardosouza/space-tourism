269cdb86d2ed15ddf13d355c225896e5583059c7
Diego Ricardo <diegoricardoweb@gmail.com>
2022-04-22 03:49:03 -0300

    Criando estilos globais com createGlobalStyle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,22 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Space Tourism</title>
+        <link rel="shortcut icon" href="/img/favicon.png" />
+        <link rel="apple-touch-icon" href="/img/favicon.png" />
+        <meta
+          name="description"
+          content="Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
