8bce3b17b850e51994639156add4448efd1f0765
Diego Ricardo <diegoricardoweb@gmail.com>
2022-04-23 03:43:41 -0300

    Criado componente Logo

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,22 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../src/styles/global';
 import theme from '../src/styles/theme'
 
+export const parameters = {
+  backgrounds: {
+    default: 'space-light',
+    values: [
+      {
+        name: 'space-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'space-dark',
+        value: theme.colors.primary
+      }
+    ]
+  }
+}
+
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
