c7ac2a5f29985371989a2f0e94ad45850a140b76
Diego Ricardo <diegoricardoweb@gmail.com>
2022-04-23 02:53:23 -0300

    Criando e configurando o theme.ts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <title>Space Tourism</title>
         <link rel="shortcut icon" href="/img/favicon.png" />
@@ -14,9 +16,15 @@ export default function App({ Component, pageProps }: AppProps) {
           name="description"
           content="Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Barlow+Condensed&family=Bellefair&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,30 @@
+export default {
+  grid: {
+    container: '111rem'
+  },
+  colors: {
+    primary: '#0B0D17',
+    secondary: '#D0D6F9',
+    white: '#FFF'
+  },
+  font: {
+    family: {
+      bellefair:
+        "Bellefair, --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+      barlow:
+        "Barlow Condensed, --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
+    },
+    light: 300,
+    normal: 400,
+    bold: 700,
+    sizes: {
+      xsmall: '1.4rem',
+      small: '1.6rem',
+      medium: '2.8rem',
+      large: '3.2rem',
+      xlarge: '5.6rem',
+      xxlarge: '10rem',
+      xxxlarge: '15rem'
+    }
+  }
+}
